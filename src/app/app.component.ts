f08bf667f6e6065dfc83f5f9f34b2f5a0e65f071
Brandon Clapp <brandonclapp@outlook.com>
2024-08-06 18:09:13 -0500

    Scaffold pages and routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'budget-app';
+  title = 'budget-app update';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { BalancePageComponent } from './balance-page/balance-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomePageComponent
+    },
+    {
+        path: 'balance',
+        component: BalancePageComponent
+    },
+    {
+        path: '**',
+        component: NotFoundPageComponent
+    }
+];
